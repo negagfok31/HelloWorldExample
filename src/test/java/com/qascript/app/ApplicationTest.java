d9abb677aae55015abc3def9f3ff29e5eb67f0ae
Abhimanyu <amansri2997@gmail.com>
2025-06-30 02:28:58 +0530

    revert changes

--- src/test/java/com/qascript/app/ApplicationTest.java
+++ src/test/java/com/qascript/app/ApplicationTest.java
@@ -11,7 +11,7 @@ public class ApplicationTest {
     public void testApplication(){
         try{
             Assert.assertEquals("Hello World!", app.getMessage());
-            System.out.println("Abhimanyu Aman Srivastav");
+            System.out.println("Abhimanyu");
         }
         catch (Exception e){
             Assert.fail("Message is  not Hello World!");
